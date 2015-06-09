eb271e7c30b7b57d1a9edeffb63e537fd54234e2
flachadriano <flachadriano@gmail.com>
2015-06-09 00:43:52 -0300

    project classes

--- 06-fundamentos-web-iv/mymony/README.md
+++ 06-fundamentos-web-iv/mymony/README.md
@@ -1,3 +1,5 @@
+Personal finance project with VRaptor
+
 Used this post as base:
 - https://pedrohosilva.wordpress.com/2015/01/01/aplicacao-web-com-vraptor-4-pt3/
 
---
+++ 06-fundamentos-web-iv/mymony/src/main/java/edu/furb/mymony/CategoriesController.java
@@ -0,0 +1,12 @@
+package edu.furb.mymony;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class CategoriesController {
+
+	public void index() {
+		System.out.println("# Olá Vraptor Controller!");
+	}
+
+}

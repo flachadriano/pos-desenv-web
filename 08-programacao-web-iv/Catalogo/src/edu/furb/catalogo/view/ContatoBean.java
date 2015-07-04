ffbad259ecfacc52bd06c3482cdb0c9679e1683f
flachadriano <flachadriano@gmail.com>
2015-07-04 15:50:43 -0300

    editing contact

--- 08-programacao-web-iv/Catalogo/src/edu/furb/catalogo/view/ContatoBean.java
+++ 08-programacao-web-iv/Catalogo/src/edu/furb/catalogo/view/ContatoBean.java
@@ -3,6 +3,7 @@ package edu.furb.catalogo.view;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.html.HtmlDataTable;
 
 import edu.furb.catalogo.model.Contato;
 import edu.furb.catalogo.persistence.ContatoDAO;
@@ -10,8 +11,9 @@ import edu.furb.catalogo.persistence.ContatoDAO;
 @ManagedBean
 public class ContatoBean {
 
-	public ContatoDAO dao = new ContatoDAO();
-	public Contato contato = new Contato();
+	private ContatoDAO dao = new ContatoDAO();
+	private Contato contato = new Contato();
+	private HtmlDataTable selecionado;
 
 	public List<Contato> getContatos() {
 		return dao.list();
@@ -19,7 +21,17 @@ public class ContatoBean {
 
 	public String criar() {
 		dao.criar(contato);
-		return "/contatos/index";
+		return "/contatos/index?face-redirect=true";
+	}
+
+	public String editar() {
+		contato = (Contato) selecionado.getRowData();
+		return "/contatos/update";
+	}
+
+	public String alterar() {
+		dao.alterar(contato);
+		return "/contatos/index?face-redirect=true";
 	}
 
 	public Contato getContato() {
@@ -28,6 +40,14 @@ public class ContatoBean {
 
 	public void setContato(Contato contato) {
 		this.contato = contato;
+	}
+
+	public HtmlDataTable getSelecionado() {
+		return selecionado;
+	}
+
+	public void setSelecionado(HtmlDataTable selecionado) {
+		this.selecionado = selecionado;
 	}
 
 }

c8e663cdc95fe40a79397bb700977fc32da9c71c
flachadriano <flachadriano@gmail.com>
2015-07-15 21:28:32 -0300

    models

--- 08-programacao-web-iv/Estoque/src/br/edu/furb/estoque/model/CategoriaProduto.java
+++ 08-programacao-web-iv/Estoque/src/br/edu/furb/estoque/model/CategoriaProduto.java
@@ -6,27 +6,16 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
-@Table(name = "produtos")
-public @Data class Produto {
+@Table(name = "categorias")
+public class CategoriaProduto {
 
 	@Id
 	@GeneratedValue
 	@Column(name = "id")
 	private int id;
 
-	@Column(name = "codigo", nullable = false)
-	private String codigo;
-
 	@Column(name = "nome", nullable = false)
 	private String nome;
-
-	@Column(name = "preco", nullable = false)
-	private Double preco;
-
-	@Column(name = "estoque", nullable = false)
-	private Double estoque;
 
 }
